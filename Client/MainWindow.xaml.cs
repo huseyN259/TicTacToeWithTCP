fe65883d9330187cf8f43947caf9c5e244d1e773
Ibra_xj86 <Ibra_xj86@itstep.edu.az>
2023-04-08 18:26:03 +0400

    Finished

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Client;
+
+public partial class MainWindow : Window
+{
+	private bool xTurn;
+	private int[,] board;
+
+	private TcpClient client;
+	private NetworkStream stream;
+	private Button[,] buttons;
+	private BinaryReader reader;
+	private BinaryWriter writer;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+
+		Initialize();
+		Task.Run(ReceiveUpdates);
+	}
+
+	private void Initialize()
+	{
+		board = new int[3, 3];
+
+		buttons = new Button[3, 3]
+		{
+			{ button00, button01, button02 },
+			{ button10, button11, button12 },
+			{ button20, button21, button22 }
+		};
+
+		client = new TcpClient("localhost", 1234);
+		stream = client.GetStream();
+		reader = new BinaryReader(stream);
+		writer = new BinaryWriter(stream);
+
+		xTurn = reader.ReadBoolean();
+
+		if (xTurn)
+			Title = "Player X";
+		else
+			Title = "Player O";
+	}
+
+	private void Button_Click(object sender, RoutedEventArgs e)
+	{
+		Button button = (Button)sender;
+		int row = Grid.GetRow(button);
+		int column = Grid.GetColumn(button);
+
+		if (xTurn)
+		{
+			button.Content = "X";
+			board[row, column] = 1;
+		}
+		else
+		{
+			button.Content = "O";
+			board[row, column] = 2;
+		}
+
+		button.IsEnabled = false;
+
+		CheckForWinner();
+		writer.Write(GetGameState());
+
+		grid.IsEnabled = false;
+	}
+
+	private void ReceiveUpdates()
+	{
+		while (true)
+		{
+			Dispatcher.Invoke(() => { grid.IsEnabled = true; });
+
+			string receivedData = reader.ReadString();
+			UpdateGameState(receivedData);
+		}
+	}
+
+	private string GetGameState()
+	{
+		string gameState = "";
+
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				gameState += board[i, j];
+
+		return gameState;
+	}
+
+	private void UpdateGameState(string gameState)
+	{
+		int index = 0;
+
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				int value = int.Parse(gameState[index].ToString());
+				board[i, j] = value;
+
+				if (value == 1)
+				{
+					Dispatcher.Invoke(() =>
+					{
+						buttons[i, j].Content = "X";
+						buttons[i, j].IsEnabled = false;
+					});
+				}
+				else if (value == 2)
+				{
+					Dispatcher.Invoke(() =>
+					{
+						buttons[i, j].Content = "O";
+						buttons[i, j].IsEnabled = false;
+					});
+				}
+				else
+				{
+					Dispatcher.Invoke(() =>
+					{
+						buttons[i, j].Content = "";
+						buttons[i, j].IsEnabled = true;
+					});
+				}
+
+				index++;
+			}
+		}
+	}
+
+	private void CheckForWinner()
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			if (board[i, 0] != 0 && board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+			{
+				ShowWinner(board[i, 0]);
+				return;
+			}
+		}
+
+		for (int i = 0; i < 3; i++)
+		{
+			if (board[0, i] != 0 && board[0, i] == board[1, i] && board[1, i] == board[2, i])
+			{
+				ShowWinner(board[0, i]);
+				return;
+			}
+		}
+
+		if (board[0, 0] != 0 && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+		{
+			ShowWinner(board[0, 0]);
+			return;
+		}
+
+		if (board[0, 2] != 0 && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+		{
+			ShowWinner(board[0, 2]);
+			return;
+		}
+
+		bool isTie = true;
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				if (board[i, j] == 0)
+				{
+					isTie = false;
+					break;
+				}
+			}
+
+			if (!isTie)
+				break;
+		}
+
+		if (isTie)
+			ShowTie();
+	}
+
+	private void ShowWinner(int player)
+	{
+		MessageBox.Show($"Player {(player == 1 ? "X" : "O")} wins!");
+		ResetGame();
+	}
+
+	private void ShowTie()
+	{
+		MessageBox.Show("It's a tie!");
+		ResetGame();
+	}
+
+	private void ResetGame()
+	{
+		board = new int[3, 3];
+
+		foreach (var element in grid.Children)
+		{
+			if (element is Button button)
+			{
+				button.Content = "";
+				button.IsEnabled = true;
+			}
+		}
+	}
+}
---
+++ Server/Program.cs
@@ -0,0 +1,105 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 1234;
+
+TcpListener listener = new TcpListener(ipAddress, port);
+Dictionary<int, TcpClient> clients = new Dictionary<int, TcpClient>();
+Dictionary<int, NetworkStream> streams = new Dictionary<int, NetworkStream>();
+Dictionary<int, int> groups = new Dictionary<int, int>();
+
+int nextClientId = 1;
+int nextGroupId = 1;
+
+Console.WriteLine("Starting server on " + ipAddress + ":" + port);
+listener.Start();
+
+Console.WriteLine("Server started. Listening for connections...");
+
+while (true)
+{
+    TcpClient client = listener.AcceptTcpClient();
+    int clientId = nextClientId++;
+    clients.Add(clientId, client);
+    NetworkStream stream = client.GetStream();
+    streams.Add(clientId, stream);
+
+    Console.WriteLine("Client connected with ID " + clientId);
+
+    int groupId = -1;
+    foreach (int otherClientId in clients.Keys)
+    {
+        if (otherClientId != clientId && !groups.ContainsKey(otherClientId))
+        {
+            groupId = nextGroupId++;
+            groups.Add(clientId, groupId);
+            groups.Add(otherClientId, groupId);
+            Console.WriteLine("Group " + groupId + " created with clients " + clientId + " and " + otherClientId);
+            BinaryWriter bw = new BinaryWriter(streams[clientId]);
+            bw.Write(true);
+            bw = new BinaryWriter(streams[otherClientId]);
+            bw.Write(false);
+            break;
+        }
+    }
+
+    if (groupId == -1)
+        Console.WriteLine("Waiting for another client to form a group...");
+
+    Task.Run(() =>
+    {
+        var reader = new BinaryReader(streams[clientId]);
+        while (true)
+        {
+            try
+            {
+                var receivedData = reader.ReadString();
+
+                foreach (int otherClientId in clients.Keys)
+                {
+                    if (otherClientId != clientId && groups.ContainsKey(otherClientId) && groups[otherClientId] == groups[clientId])
+                    {
+                        var writer = new BinaryWriter(streams[otherClientId]);
+                        writer.Write(receivedData);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Client with ID " + clientId + " disconnected.");
+
+                if (groups.ContainsKey(clientId))
+                {
+                    int groupId = groups[clientId];
+                    groups.Remove(clientId);
+
+                    bool groupEmpty = true;
+                    foreach (int otherClientId in groups.Keys)
+                    {
+                        if (groups[otherClientId] == groupId)
+                        {
+                            groupEmpty = false;
+                            break;
+                        }
+                    }
+
+                    if (groupEmpty)
+                    {
+                        Console.WriteLine("Group " + groupId + " is now empty and has been removed.");
+                        
+                        foreach (int otherClientId in groups.Keys.ToList())
+                            if (groups[otherClientId] > groupId)
+                                groups[otherClientId]--;
+                        
+                        nextGroupId--;
+                    }
+                }
+
+                clients.Remove(clientId);
+                streams.Remove(clientId);
+                return;
+            }
+        }
+    });
+}
